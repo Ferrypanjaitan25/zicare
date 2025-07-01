4ee4e67556705c78bfd1bba881b11e8261a6d376
Christoffelnapitupulu27 <christoffeltheofani@gmail.com>
2025-07-01 12:17:56 +0700

    fix

--- src/lib/stores/students.js
+++ src/lib/stores/students.js
@@ -12,10 +12,66 @@ export const studentsStore = writable([
   
   {
     id: 2,
-    nim: '14S23024',
-    nama: 'Cardolan Sinaga',
-    prodi: 'Teknik Elektro',
-    email: '14S23024@students.del.ac.id',
+    nim: '11S22013',
+    nama: 'Christoffel Theofani Napitupulu',
+    prodi: 'Informatika',
+    email: 'ifs22013@students.del.ac.id',
     noHp: '082112345678'
+  },
+  {
+    id: 3,
+    nim: '14S22040',
+    nama: 'Stevanus Hutabarat',
+    prodi: 'Teknik Elektro',
+    email: 'els22040@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id: 4,
+    nim: '21S22040',
+    nama: 'Rejeki Sitorus',
+    prodi: 'Manajemen Rekayasa',
+    email: 'mrs22040@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id: 5,
+    nim: '13322025',
+    nama: 'Morlin Siahaan',
+    prodi: 'Teknik Komputer',
+    email: 'ce22025@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id: 6,
+    nim: '22S23061',
+    nama: 'Johan Hutabarat',
+    prodi: 'Teknik Metalurgi',
+    email: 'tms22061@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id:7,
+    nim: '31S22040',
+    nama: 'Stevanus Hutabarat',
+    prodi: 'Teknik Bioproses',
+    email: 'bps22040@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id: 8,
+    nim: '11422040',
+    nama: 'Stevanus Hutabarat',
+    prodi: 'Teknik Rekaya Perangkat Lunak',
+    email: 'if422040@students.del.ac.id',
+    noHp: '081234567890'
+  },
+  {
+    id: 9,
+    nim: '11322024',
+    nama: 'Stevanus Hutabarat',
+    prodi: 'Teknik Elektro',
+    email: 'ti22040@students.del.ac.id',
+    noHp: '081234567890'
   }
 ]);
